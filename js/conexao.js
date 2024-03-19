105abb91d366b977c07a81b9844b1c3a5650aede
Amanda Oliveira <100137341+amandazzoc@users.noreply.github.com>
2024-03-19 16:16:31 -0300

    começo js

---
+++ js/conexao.js
@@ -0,0 +1,21 @@
+const setBanco = (banco) => {
+    localStorage.setItem(bd-mario, banco);
+};
+
+const getBanco = () => {
+    return JSON.parse(localStorage.getItem(bd-mario) ?? []);
+};
+
+const bancoTemp = (nome, moedas, estrelas, tempo, pontuacao) => {
+    let banco = getBranco();
+
+    let dados = {
+        nomeJogador: nome,
+        moedasJogador: moedas,
+        estrelasJogador: estrelas,
+        tempoJogador: tempo,
+        pontuacaoJogador: pontuacao
+    };
+
+    banco.unshift[]
+};
